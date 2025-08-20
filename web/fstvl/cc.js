0acecb7a9a5c783024c2e5468d0f9599eee8001d
dodypribadi5 <dodypribadi5@gmail.com>
2025-08-20 15:25:32 +0700

    Delete web/fstvl directory

--- web/fstvl/cc.js
+++
@@ -1,7 +0,0 @@
-let tglNow = new Date();
-let waktuBatas = new Date("2025-08-26T23:59:59");
-if (tglNow.getTime() >= waktuBatas.getTime()) {
-  document.location.href = 'https://suspend-web.pages.dev';
-} else {
-  console.log('Server aktif');
-}
